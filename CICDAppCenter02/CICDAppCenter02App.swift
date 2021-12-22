249a86b29047dea40488a7ec45879a25a12fc8fb
Hadi Razzak <hadi_razzak@huntsman.com>
2021-12-22 14:47:52 -0600

    Initial commit

---
+++ CICDAppCenter02/CICDAppCenter02App.swift
@@ -0,0 +1,29 @@
+//
+/*
+ ********************************************************************************
+ ************************ HUNTSMAN INTERNATIONAL LLC ****************************
+ ********************** Headquarters: The Woodlands, TX *************************
+ ********************************************************************************
+ * Author                : Bumpy
+ * Created On            : 12/22/21
+ * Project Name          : CICDAppCenter02
+ * Huntsman Change Number:
+ * Bespoke Number        :
+ * Project Description   :
+ *                       :
+ *                       :
+ *
+ * 
+ ********************************************************************************
+*/
+
+import SwiftUI
+
+@main
+struct CICDAppCenter02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDAppCenter02/ContentView.swift
@@ -0,0 +1,33 @@
+//
+/*
+ ********************************************************************************
+ ************************ HUNTSMAN INTERNATIONAL LLC ****************************
+ ********************** Headquarters: The Woodlands, TX *************************
+ ********************************************************************************
+ * Author                : Bumpy
+ * Created On            : 12/22/21
+ * Project Name          : CICDAppCenter02
+ * Huntsman Change Number:
+ * Bespoke Number        :
+ * Project Description   :
+ *                       :
+ *                       :
+ *
+ * 
+ ********************************************************************************
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
